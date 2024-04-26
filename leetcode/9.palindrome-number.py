e77a2160ea14f9714873ea7175b2a4579c8ba1b8
Makarios Nassef <makarios059@gmail.com>
2024-04-26 17:12:14 +0300

    [9] Palindrome Number

--- leetcode/9.palindrome-number.py
+++ leetcode/9.palindrome-number.py
@@ -7,6 +7,18 @@
 # @lc code=start
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        
+        # return (str(x) == str(x)[::-1])
+
+        if x < 0:
+            return (False)
+        s = str(x)
+        reverse = s[::-1]
+        if s==reverse:
+            return (True)
+        else:
+            return (False)
+
+
+            
 # @lc code=end
 
