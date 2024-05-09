1243b8077ca7e475e9d5f1d4b50ab5bafa1e0a1a
Makarios Nassef <makarios059@gmail.com>
2024-05-09 23:45:52 +0300

    [70] Climbing Stairs

---
+++ leetcode/70.climbing-stairs.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=70 lang=python3
+#
+# [70] Climbing Stairs
+#
+
+# @lc code=start
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 3: return n
+
+        prev1 = 3
+        prev2 = 2
+        cur = 0
+
+        for _ in range(3, n):
+            cur = prev1 + prev2
+            prev2 = prev1
+            prev1 = cur
+        
+        return cur
+# @lc code=end
+
